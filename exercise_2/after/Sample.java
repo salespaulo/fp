6ff1047293da8104dff286718e405a1799871b2d
psales <salespaulo08@gmail.com>
2016-12-27 13:21:07 -0200

    ...

--- exercise_2/after/Sample.java
+++ exercise_2/after/Sample.java
@@ -4,17 +4,19 @@ import java.util.stream.Stream;
 
 class Sample {
 	public static boolean isPrime(int number) {
-		return number > 1 && IntStream.range(2, number).noneMatch(i -> number % i == 0);
+		return number > 1 && 
+				IntStream.range(2, number) // exclusivo -> exclui o number
+				.noneMatch(i -> number % i == 0);
 	}
 
 	public static double computeSumOfSqrtOfPrimes(int start, int count) {
-		// Interface de Função Identidade: F<A> identity();
+		// Interface de Função Identidade: <T> UnaryOperator<T> identity();
 		final UnaryOperator<Integer> incrStart = i -> Integer.valueOf(i + 1);
 
-		return Stream.iterate(start, incrStart)
+		return Stream.iterate(start, incrStart) // Infinite Series
 				.filter(Sample::isPrime)
 				.mapToDouble(Math::sqrt)
-				.limit(count) // Infinite Series
+				.limit(count) 
 				.sum(); // Lazy Evaluation
 	}
 
